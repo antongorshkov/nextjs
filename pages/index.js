2f9ebaf4dc5d5ea9bf42d4f57ba97285c7642902
Anton Gorshkov <antong@gmail.com>
2021-07-07 14:50:54 -0400

    Update index.js
    Just changing some text

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,13 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Welcome to Tribefolio!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://tribefolio.com">Tribefolio!</a>
         </h1>
 
         <p className={styles.description}>
